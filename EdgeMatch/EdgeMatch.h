997a282db7d6e749b5329d36cb1688361792637c
njit_77 <512424964@qq.com>
2020-03-06 19:59:26 +0800

    1、创建项目;

---
+++ EdgeMatch/EdgeMatch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+#ifdef _WIN64
+
+#if _DEBUG
+#pragma comment (lib,"opencv_world420d.lib")
+#else
+#pragma comment (lib,"opencv_world420.lib")
+#endif
+
+#else
+#endif
+
+class EdgeMatch
+{
+public:
+	
+
+private:
+
+};
