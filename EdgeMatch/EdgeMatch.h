34c33f667d75c983f21b39a02b87ba9d69e03193
njit_77 <512424964@qq.com>
2020-03-07 22:42:17 +0800

    1、微改函数; 2、增加剔除点;

--- EdgeMatch/EdgeMatch.h
+++ EdgeMatch/EdgeMatch.h
@@ -14,11 +14,20 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 
-#include <objbase.h>
+#include <guiddef.h>
 #include <vector>
 
 
-#define PryNumber 3
+#define PryNum 3
+
+#define CONVERT_STR_2_GUID(str, guid) do\
+{\
+	sscanf_s(str, "%8x-%4x-%4x-%2x%2x-%2x%2x%2x%2x%2x%2x",\
+	&(guid.Data1),&(guid.Data2),&(guid.Data3),\
+	&(guid.Data4[0]),&(guid.Data4[1]),&(guid.Data4[2]),&(guid.Data4[3]),\
+	&(guid.Data4[4]),&(guid.Data4[5]),&(guid.Data4[6]),&(guid.Data4[7]));\
+}while (0);
+
 
 struct EdgeModelBaseInfo
 {
@@ -34,6 +43,7 @@ struct EdgeModelBaseInfo
 	/*1/magnitude*/
 	float magnitudeN;
 
+	/*边缘点*/
 	cv::Point contour;
 
 	EdgeModelBaseInfo()
@@ -52,15 +62,54 @@ struct EdgeModelInfo
 
 	int MinGray;
 	int MaxGray;
-	int PryNum;
-	std::vector<std::vector<EdgeModelBaseInfo>> EdgeModelBaseInfos;
+	int PryNumber;
+	float Score;
+	float StartAngle;
+	float EndAngle;
+	float StepAngle;
+	float Greediness;
+	int* EdgeModelBaseInfoSize;
+	EdgeModelBaseInfo** EdgeModelBaseInfos;
 
 	EdgeModelInfo()
 	{
+		ModelID = { 0 };
 		MinGray = 0;
 		MaxGray = 0;
-		PryNum = PryNumber;
+		PryNumber = PryNum;
+		Score = 0.5;
+		StartAngle = -45;
+		EndAngle = 45;
+		StepAngle = 1;
+		Greediness = 0.9;
+		EdgeModelBaseInfoSize = nullptr;
+		EdgeModelBaseInfos = nullptr;
 	}
+
+	~EdgeModelInfo()
+	{
+		DeleteData();
+	}
+
+	void DeleteData()
+	{
+		if (EdgeModelBaseInfoSize != nullptr)
+		{
+			delete[] EdgeModelBaseInfoSize;
+			EdgeModelBaseInfoSize = nullptr;
+		}
+		if (EdgeModelBaseInfos != nullptr)
+		{
+			for (size_t i = 0; i < PryNumber; i++)
+			{
+				delete[] EdgeModelBaseInfos[i];
+				EdgeModelBaseInfos[i] = nullptr;
+			}
+			delete[] EdgeModelBaseInfos;
+			EdgeModelBaseInfos = nullptr;
+		}
+	}
+
 };
 
 class EdgeMatch
@@ -78,23 +127,48 @@ private:
 	{
 		if (ModelInfo != nullptr)
 		{
-			for (size_t i = 0; i < ModelInfo->EdgeModelBaseInfos.size(); i++)
-			{
-				for (size_t j = 0; j < ModelInfo->EdgeModelBaseInfos[i].size(); j++)
-				{
-					ModelInfo->EdgeModelBaseInfos[i].clear();
-				}
-				ModelInfo->EdgeModelBaseInfos.clear();
-			}
+			ModelInfo->DeleteData();
 			delete ModelInfo;
 			ModelInfo = nullptr;
 		}
 	}
 
-public:
-	int create_edge_model_path(const char* picPath, const char* modelID, int minGray, int maxGray, int pryNum);
+private:
+	
+	/** @brief 构建金字塔模型
+
+		@anchor pryImage
+
+		@param srcImage 原始图像
+		@param pryImage 金字塔模型
+		@param pryNum 金字塔层数
+	*/
 	void pryImage(cv::Mat srcImage, std::vector<cv::Mat>& pryImage, int pryNum);
-	void createGrad(cv::Mat pryCanny, cv::Mat prySobelX, cv::Mat prySobelY, std::vector<EdgeModelBaseInfo>& data);
+	
+	/** @brief 生成梯度信息
+
+		@anchor createGradInfo
+
+		@param pryCanny canny处理后图像
+		@param prySobelX Sobel X方向处理后图像
+		@param prySobelY Sobel Y方向处理后图像
+		@param gradData 梯度信息集合
+	*/
+	void createGradInfo(cv::Mat pryCanny, cv::Mat prySobelX, cv::Mat prySobelY, std::vector<EdgeModelBaseInfo>& gradData);
+
+	/** @brief 剔除轮廓点
+
+		@anchor deleteContourPoints
+
+		@param gradData 梯度信息集合
+	*/
+	void deleteContourPoints(std::vector<EdgeModelBaseInfo>& gradData);
+
+public:
+
+	int create_edge_model_path(const char* picPath, const char* modelID, int minGray, int maxGray, int pryNum,
+		float score, float startAngle, float endAngle, float stepAngle, float greediness);
+	
 
 public:
 
@@ -103,4 +177,5 @@ public:
 		static EdgeMatch m_instance;
 		return m_instance;
 	}
+
 };
